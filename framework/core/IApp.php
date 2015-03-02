c919fb6dd4d9adb684c745dfd7a0a51b65e6ad75
zhouzhichao <zzc123@foxmail.com>
2015-03-02 09:31:57 +0800

    add functions

--- framework/core/IApp.php
+++ framework/core/IApp.php
@@ -3,6 +3,7 @@
 namespace zz\core;
 interface IApp {
     public function init();
+    public function analyzeRoute();
     public function run();
     public function shutdown();
     public function readConfig();
